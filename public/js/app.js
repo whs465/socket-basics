02771b02345c733a5db5a419dd7c9603ab42b926
whs465 <whs465@gmail.com>
2016-02-08 11:22:51 -0500

    Add fornt-end form to summit message

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,14 @@ socket.on('message',function  (message) {
     console.log('new message');
     console.log(message.text);
 });
+
+var $form=jQuery('#message-form');
+$form.on('submit',function  (event) {
+    event.preventDefault();
+    var $message = $form.find('input[name=message]');
+    socket.emit('message',{
+        text:$message.val()
+    });
+
+    $message.val('');
+});
--- server.js
+++ server.js
@@ -10,9 +10,9 @@ io.on('connection', function(socket) {
     console.log('User connected via socket.io');
 
     socket.on('message', function(message) {
-        console.log('Message received' + message.text);
+        console.log('Message received: ' + message.text);
         socket.broadcast.emit('message', message);
-    });
+    }); 
 
     socket.emit('message', {
         text: 'welcome to the chat app!'
