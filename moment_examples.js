5009b855154a7da2a51853f12d8d78157a7e14dd
whs465 <whs465@gmail.com>
2016-02-08 17:03:09 -0500

    Add moment example and file

---
+++ moment_examples.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+var now = moment();
+
+var timestamp = 1444247486704;
+var timestampMoment= moment.utc(timestamp);
+
+console.log(timestampMoment.local().format('h:mm a'));
+
