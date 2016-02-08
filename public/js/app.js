986b9951d7f838cf6f6d80f01a7679328433f1cc
whs465 <whs465@gmail.com>
2016-02-08 10:20:54 -0500

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,8 @@ var socket = io();
 socket.on('connect',function  () {
     console.log('connected to socket.io server!');
 });
+
+socket.on('message',function  (message) {
+    console.log('new message');
+    console.log(message.text);
+});
