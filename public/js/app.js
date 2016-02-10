ed114a256bd645c256fae6a3614ef31a5dc8d724
William Hernandez Sierra <whs465@gmail.com>
2016-02-10 17:49:30 -0500

    Add styles for chat app

--- public/js/app.js
+++ public/js/app.js
@@ -4,29 +4,31 @@ var socket = io();
 
 console.log(name + ' wants to join ' + room);
 jQuery('.room-title').text(room);
-socket.on('connect', function () {
+socket.on('connect', function() {
     console.log('Connected to socket.io server!');
-    socket.emit('joinRoom',{
-        name:name,
-        room:room
+    socket.emit('joinRoom', {
+        name: name,
+        room: room
     });
 });
 
-socket.on('message', function (message) {
+socket.on('message', function(message) {
     var momentTimestamp = moment.utc(message.timestamp);
-    var $message = jQuery('.messages');
+    var $messages = jQuery('.messages');
+    var $message = jQuery('<li class="list-group-item"></li>');
 
     console.log('New message:');
     console.log(message.text);
 
     $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
     $message.append('<p>' + message.text + '</p>');
+    $messages.append($message);
 });
 
 // Handles submitting of new message
 var $form = jQuery('#message-form');
 
-$form.on('submit', function (event) {
+$form.on('submit', function(event) {
     event.preventDefault();
 
     var $message = $form.find('input[name=message]');
