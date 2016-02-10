706dc7d049f49d792f8e297ba6ef3bcca9dac1f6
whs465 <whs465@gmail.com>
2016-02-09 19:38:53 -0500

    Add support for chat rooms

--- public/js/app.js
+++ public/js/app.js
@@ -3,9 +3,13 @@ var room = getQueryVariable('room');
 var socket = io();
 
 console.log(name + ' wants to join ' + room);
-
+jQuery('.room-title').text(room);
 socket.on('connect', function () {
-    console.log('Conncted to socket.io server!');
+    console.log('Connected to socket.io server!');
+    socket.emit('joinRoom',{
+        name:name,
+        room:room
+    });
 });
 
 socket.on('message', function (message) {
--- server.js
+++ server.js
@@ -7,14 +7,25 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+var clientInfo = {};
+
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
+    socket.on('joinRoom', function (req) {
+        clientInfo[socket.id] = req;
+        socket.join(req.room);
+        socket.broadcast.to(req.room).emit('message', {
+            name: 'System',
+            text: req.name + ' has joined!',
+            timestamp: moment().valueOf()
+        });
+    });
 
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
 
         message.timestamp = moment().valueOf();
-        io.emit('message', message);
+        io.to(clientInfo[socket.id].room).emit('message', message);
     });
 
     // timestamp property - JavaScript timestamp (milliseconds)
