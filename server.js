483d154dbee87a21475eec6d6e1d077b9baf7ca2
whs465 <whs465@gmail.com>
2016-02-09 20:56:36 -0500

    Add disconect event and message

--- server.js
+++ server.js
@@ -11,6 +11,21 @@ var clientInfo = {};
 
 io.on('connection', function (socket) {
     console.log('User connected via socket.io!');
+    socket.on('disconnect', function () {
+        var userData=clientInfo[socket.id];
+        if (typeof userData!== 'undefined') {
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message',{
+                name:'System',
+                text:userData.name + ' has left.',
+                timestamp: moment().valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
+        else {
+            
+        }
+    })
     socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
@@ -24,7 +39,7 @@ io.on('connection', function (socket) {
     socket.on('message', function (message) {
         console.log('Message received: ' + message.text);
 
-        message.timestamp = moment().valueOf();
+        message.timestamp = moment().valueOf()
         io.to(clientInfo[socket.id].room).emit('message', message);
     });
 
